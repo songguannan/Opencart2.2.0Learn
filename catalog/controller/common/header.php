fb9208a92647a8a3bbef106b51fde34120a90992
songguannan <405038777@qq.com>
2016-10-31 15:52:32 +0800

    Opencart2.2.0Learn First Virsion

---
+++ catalog/controller/common/header.php
@@ -0,0 +1,162 @@
+<?php
+class ControllerCommonHeader extends Controller {
+	public function index() {
+		// Analytics
+		$this->load->model('extension/extension');
+
+		$data['analytics'] = array();
+
+		$analytics = $this->model_extension_extension->getExtensions('analytics');
+
+		foreach ($analytics as $analytic) {
+			if ($this->config->get($analytic['code'] . '_status')) {
+				$data['analytics'][] = $this->load->controller('analytics/' . $analytic['code'], $this->config->get($analytic['code'] . '_status'));
+			}
+		}
+//?这个analytics不知道什么作用，如果是Google分析监控可以删除
+//决定server变量的值
+		if ($this->request->server['HTTPS']) {
+			$server = $this->config->get('config_ssl');
+		} else {
+			$server = $this->config->get('config_url');
+		}
+//?常量的定义在什么地方
+//添加icon的链接
+		if (is_file(DIR_IMAGE . $this->config->get('config_icon'))) {
+		//addlink只是添加链接，可能要等待调用getlink，第一个参数是链接，第二个参数是说明
+			$this->document->addLink($server . 'image/' . $this->config->get('config_icon'), 'icon');
+		}
+//没设置就调用了
+//?这些数据在哪设置的
+//数据获取
+		$data['title'] = $this->document->getTitle();
+
+		$data['base'] = $server;
+		$data['description'] = $this->document->getDescription();
+		$data['keywords'] = $this->document->getKeywords();
+		$data['links'] = $this->document->getLinks();
+		$data['styles'] = $this->document->getStyles();
+		$data['scripts'] = $this->document->getScripts();
+		$data['lang'] = $this->language->get('code');
+		$data['direction'] = $this->language->get('direction');
+//?config在什么地方设置的
+//logo的设置
+		$data['name'] = $this->config->get('config_name');
+
+		if (is_file(DIR_IMAGE . $this->config->get('config_logo'))) {
+			$data['logo'] = $server . 'image/' . $this->config->get('config_logo');
+		} else {
+			$data['logo'] = '';
+		}
+//加载控制器
+		$this->load->language('common/header');
+
+		$data['text_home'] = $this->language->get('text_home');
+
+		// Wishlist
+		if ($this->customer->isLogged()) {
+			$this->load->model('account/wishlist');
+
+			$data['text_wishlist'] = sprintf($this->language->get('text_wishlist'), $this->model_account_wishlist->getTotalWishlist());
+		} else {
+			$data['text_wishlist'] = sprintf($this->language->get('text_wishlist'), (isset($this->session->data['wishlist']) ? count($this->session->data['wishlist']) : 0));
+		}
+
+		$data['text_shopping_cart'] = $this->language->get('text_shopping_cart');
+		$data['text_logged'] = sprintf($this->language->get('text_logged'), $this->url->link('account/account', '', true), $this->customer->getFirstName(), $this->url->link('account/logout', '', true));
+
+		$data['text_account'] = $this->language->get('text_account');
+		$data['text_register'] = $this->language->get('text_register');
+		$data['text_login'] = $this->language->get('text_login');
+		$data['text_order'] = $this->language->get('text_order');
+		$data['text_transaction'] = $this->language->get('text_transaction');
+		$data['text_download'] = $this->language->get('text_download');
+		$data['text_logout'] = $this->language->get('text_logout');
+		$data['text_checkout'] = $this->language->get('text_checkout');
+		$data['text_category'] = $this->language->get('text_category');
+		$data['text_all'] = $this->language->get('text_all');
+
+		$data['home'] = $this->url->link('common/home');
+		$data['wishlist'] = $this->url->link('account/wishlist', '', true);
+		$data['logged'] = $this->customer->isLogged();
+		$data['account'] = $this->url->link('account/account', '', true);
+		$data['register'] = $this->url->link('account/register', '', true);
+		$data['login'] = $this->url->link('account/login', '', true);
+		$data['order'] = $this->url->link('account/order', '', true);
+		$data['transaction'] = $this->url->link('account/transaction', '', true);
+		$data['download'] = $this->url->link('account/download', '', true);
+		$data['logout'] = $this->url->link('account/logout', '', true);
+		$data['shopping_cart'] = $this->url->link('checkout/cart');
+		$data['checkout'] = $this->url->link('checkout/checkout', '', true);
+		$data['contact'] = $this->url->link('information/contact');
+		$data['telephone'] = $this->config->get('config_telephone');
+
+		// Menu
+		$this->load->model('catalog/category');
+//加载模型
+		$this->load->model('catalog/product');
+//加载模型
+//声名数据
+		$data['categories'] = array();
+//通过模型从数据库取数据
+		$categories = $this->model_catalog_category->getCategories(0);
+//遍历数据
+		foreach ($categories as $category) {
+		//?这个top代表的意义是什么
+		//!如果top值为真,那么就是顶级栏目
+			if ($category['top']) {
+				// Level 2
+				//声明变量数组
+				$children_data = array();
+   //如果top为真才取每个栏目ID从数据库取栏目数据，取出的是二级目录的数据
+				$children = $this->model_catalog_category->getCategories($category['category_id']);
+   //
+				foreach ($children as $child) {
+					$filter_data = array(
+						'filter_category_id'  => $child['category_id'],
+						'filter_sub_category' => true
+					);
+
+					$children_data[] = array(
+						'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+						'href'  => $this->url->link('product/category', 'path=' . $category['category_id'] . '_' . $child['category_id'])
+					);
+				}
+
+				// Level 1
+				$data['categories'][] = array(
+					'name'     => $category['name'],
+					'children' => $children_data,
+					'column'   => $category['column'] ? $category['column'] : 1,
+					'href'     => $this->url->link('product/category', 'path=' . $category['category_id'])
+				);
+			}
+		}
+
+		$data['language'] = $this->load->controller('common/language');
+		$data['currency'] = $this->load->controller('common/currency');
+		$data['search'] = $this->load->controller('common/search');
+		$data['cart'] = $this->load->controller('common/cart');
+
+		// For page specific css
+		if (isset($this->request->get['route'])) {
+			if (isset($this->request->get['product_id'])) {
+				$class = '-' . $this->request->get['product_id'];
+			} elseif (isset($this->request->get['path'])) {
+				$class = '-' . $this->request->get['path'];
+			} elseif (isset($this->request->get['manufacturer_id'])) {
+				$class = '-' . $this->request->get['manufacturer_id'];
+			} elseif (isset($this->request->get['information_id'])) {
+				$class = '-' . $this->request->get['information_id'];
+			} else {
+				$class = '';
+			}
+
+			$data['class'] = str_replace('/', '-', $this->request->get['route']) . $class;
+		} else {
+			$data['class'] = 'common-home';
+		}
+
+		return $this->load->view('common/header', $data);
+	}
+}
